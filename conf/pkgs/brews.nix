6db730860acea02d4c71cf7630d3024f513e87ac
Quentin Machu <quentin@bitmex.com>
2025-02-17 19:15:42 +0800

    Tenv is broken asf

--- conf/pkgs/brews.nix
+++ conf/pkgs/brews.nix
@@ -4,8 +4,6 @@ let
     common = [
         "mas"          # let nix install mac app store apps
         "pam-reattach" # used to enable pam touchid in tmux
-
-        "tenv"       # tofu/terraform/terragrunt version manager; nixpkg doesn't create proxy binaries        
         "aws-shell"
         "ykman"
     ];
